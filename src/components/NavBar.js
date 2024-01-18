8f4de2694cb91f92415915e71a25eefaae18a6e9
Henrique Almeida <marceloh500@gmail.com>
2024-01-18 07:43:19 -0300

    pontuacao

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdNotificationsOutline } from "react-icons/io";
+import { BsCoin } from "react-icons/bs";
 
 
 
@@ -12,10 +13,18 @@ function Navbar({onClick}){
         <div className="flex py-4 w-screen  lg:bg-slate-800 text-white fixed z-20 ">
             
             <div className="  flex flex-1 justify-between items-center px-5 ">
+
                 <span onClick={onClick} className="text-xl cursor-pointer lg:bg-transparent lg:p-0 bg-slate-800 rounded-full p-3"><GiHamburgerMenu /></span>
                 <p className="font-semibold text-lg tracking-widest hidden lg:inline-flex">DBV MASTER</p>
-                <div>
-                <span className="text-2xl"><IoMdNotificationsOutline /></span>
+               <div className="bg-slate-800 w-1/3 px-4 py-2 flex gap-1 items-center justify-center cursor-pointer rounded-full lg:hidden">
+                
+               <span className="text-2xl text-yellow-500 "><BsCoin /></span>
+                <p className="text-2xl">143</p>
+                
+               </div>
+                <div className="flex">
+                
+                <span className="text-2xl items-center cursor-pointer"><IoMdNotificationsOutline /></span>
                 </div>
                 
             </div>
