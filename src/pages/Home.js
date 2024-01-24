be7d11a8f3fb53917575a9e109774a06146af97b
Henrique Almeida <marceloh500@gmail.com>
2024-01-24 05:28:15 -0300

    alinhamento perfil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import fotoPerfil from './../img/fotos/henrique.png'
 
 function Home(){
     return(
-        <div className="min-h-screen bg-ceuDia bg-cover bg-no-repeat pt-24 px-20 lg:px-5 grid grid-cols-1 lg: grid-cols-2 content-start border border-red-400">
+        <div className="min-h-screen bg-ceuDia bg-cover bg-no-repeat pt-24 lg:px-5 flex flex-col justify-start items-center   lg:grid grid-cols-2 lg:content-start border border-red-400">
             <PerfilHome foto={fotoPerfil} nome="HENRIQUE" cargo="Secretário" estrelas={estrela7}/>
             <div className="p-1"></div>
           
